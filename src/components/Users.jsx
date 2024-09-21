7aec8cf4285001e83990c06d4d51fca0990ce023
UzairAhmed87 <uzairahmed1574@gmail.com>
2024-09-21 11:32:43 -0700

    connect backend

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,28 +1,34 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Button } from "./Button"
-
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 export const Users = () => {
     // Replace with backend call
-    const [users, setUsers] = useState([{
-        firstName: "Uzair",
-        lastName: "Ahmed",
-        _id: 1
-    }]);
-
+    const [users, setUsers] = useState([]);
+    const [filter,setFilter]=useState("")
+   
+useEffect(()=>{
+    axios.get("http://localhost:3000/api/v1/user/bulk?filter="+filter)
+    .then(res=>{
+        setUsers(res.data.user); 
+    })
+},[filter])
     return <div className="mx-4">
         <div className="font-bold mt-6 text-lg">
             Users
         </div>
         <div className="my-2">
-            <input type="text" placeholder="Search users..." className="w-full px-2 py-1 border rounded border-slate-200"></input>
+            <input onChange={(e)=>{setFilter(e.target.value)}} type="text" placeholder="Search users..." className="w-full px-2 py-1 border rounded border-slate-200"></input>
         </div>
         <div>
-            {users.map(user => <User user={user} />)}
+            {users.map(user => <User key={user._id} user={user} />)}
         </div>
         </div>
 }
 
 function User({user}) {
+    const navigate = useNavigate()
     return <div className="flex justify-between">
         <div className="flex">
             <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
@@ -38,7 +44,9 @@ function User({user}) {
         </div>
 
         <div className="flex flex-col justify-center h-ful">
-            <Button label={"Send Money"} />
+            <Button onClick={(e)=>{
+                navigate("/send?id="+user._id+"&name="+user.firstName)
+            }} label={"Send Money"} />
         </div>
     </div>
 }
--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -4,18 +4,34 @@ import { SubHeading } from '../components/SubHeading'
 import { InputBox } from '../components/InputBox'
 import { Button } from '../components/Button'
 import { BottomWarning } from '../components/BottomWarning'
-
+import { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 function Signin() {
+  const [username,setUserName] = useState("")
+  const [password,setPassword] = useState("")
+  const navigate = useNavigate()
   return (
     <div className="bg-slate-300 h-screen flex justify-center">
       <div className='flex flex-col justify-center'>
         <div className='rounded-lg bg-white w-80 text-center p-2 h-max px-4'>
 <Heading label={"Sign In"}/>
 <SubHeading label={"Enter your credentials to access your account"}/>
-<InputBox label={"Email"} placeholder={"johndoe@mail.com"}/>
-<InputBox label={"Password"}/>
+<InputBox onChange={(e)=>setUserName(e.target.value)} label={"Email"} placeholder={"johndoe@mail.com"}/>
+<InputBox onChange={(e)=>setPassword(e.target.value)} label={"Password"}/>
 <div className='pt-4'>
-<Button label={"Sign In"}/>
+<Button onClick={()=>{
+  axios.post("http://localhost:3000/api/v1/user/signin",{
+    username,
+    password
+  }).then(res => {
+    localStorage.setItem("token",res.data.token)
+    alert("Login successful")
+  navigate("/dashboard")
+  }
+  ).catch(err => {console.log(err)}
+  )
+}} label={"Sign In"}/>
 </div>
 
 <BottomWarning label={"Don't have an account?"} buttonText={"Sign Up"} to={"/dashboard"}/>
